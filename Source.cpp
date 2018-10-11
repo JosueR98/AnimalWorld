97b575369efdb3babe5812e6efa0c652cb59427c
JosueR98 <joracha98@hotmail.com>
2018-10-10 23:56:13 -0600

    CambioPrueba

--- Source.cpp
+++ Source.cpp
@@ -35,7 +35,7 @@ int main() {
 	}
 	cout << "Pensaste en : " << actual->dato.getCadena() << endl;
 
-	// Actualizado nuevo 22:07 xddsadasbgf
+	// Actualizado nuevo 22:07 sversequen
 
 	system("pause");
 
